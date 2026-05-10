e6027b2f372e5659ac3a427587b06eba69deabbc
Worker <ipz243_yaiv@student.ztu.edu.ua>
2026-05-10 22:40:32 +0300

    refactor

--- TexasHoldemWPF/ViewModels/GameBettingManager.cs
+++ TexasHoldemWPF/ViewModels/GameBettingManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using TexasHoldemWPF.Enums;
 using TexasHoldemWPF.Models.Entities;
+using TexasHoldemWPF.Models.Strategies; 
 
 namespace TexasHoldemWPF.ViewModels
 {
@@ -11,16 +12,6 @@ namespace TexasHoldemWPF.ViewModels
     {
         private readonly GameViewModel _context;
         private bool _raiseOccurredInRound = false;
-
-        private const double HALF_POT = 0.5;
-        private const double TWO_THIRDS_POT = 0.666;
-        private const double THREE_QUARTERS_POT = 0.75;
-        private const double RAISE_MULTIPLIER_2 = 2;
-        private const double RAISE_MULTIPLIER_3 = 3;
-        private const double RAISE_MULTIPLIER_4 = 4;
-        private const double BIG_BLIND_MULTIPLIER_2_5 = 2.5;
-        private const double BIG_BLIND_MULTIPLIER_4 = 4;
-        private const double BIG_BLIND_MULTIPLIER_6 = 6;
 
         public GameBettingManager(GameViewModel context)
         {
@@ -40,7 +31,6 @@ namespace TexasHoldemWPF.ViewModels
             if (!_context.CanAct) return;
 
             int betAmount = Math.Min(GameViewModel.DEFAULT_BET_AMOUNT, _context.PlayerBalance);
-
             _context.CanAct = false;
             ExecuteBet(betAmount);
             _ = _context.BotManager.StartBotActions();
@@ -48,52 +38,23 @@ namespace TexasHoldemWPF.ViewModels
 
         private void ExecuteBet(int amount)
         {
-            _context.PlayerBalance -= amount;
-            _context.PotSize += amount;
+            UpdateBalances(amount);
             _context.CurrentBet = amount;
-            _context.GetPlayer(0).Balance = _context.PlayerBalance;
-            _context.GetPlayer(0).CurrentBet = amount;
-            _context.GetPlayer(0).LastAction = $"Bet ${amount}";
-            _context.UpdateCallCheckButtonText();
+            UpdatePlayerAction(0, amount, $"Bet ${amount}");
         }
 
         public void ShowRaiseMenu()
         {
             if (!_context.CanAct) return;
 
-            if (_context.CurrentBet == 0)
-            {
-                _context.RaiseOptions = new List<int> {
-                    (int)(_context.BigBlindAmount * BIG_BLIND_MULTIPLIER_2_5),
-                    _context.BigBlindAmount * (int)BIG_BLIND_MULTIPLIER_4,
-                    _context.BigBlindAmount * (int)BIG_BLIND_MULTIPLIER_6,
-                    _context.PlayerBalance
-                };
-                _context.RaiseOptionsText = "2.5BB|4BB|6BB|ALL IN";
-            }
-            else
-            {
-                if (_context.CurrentBet == 0)
-                {
-                    _context.RaiseOptions = new List<int> {
-                        (int)(_context.PotSize * HALF_POT),
-                        (int)(_context.PotSize * TWO_THIRDS_POT),
-                        (int)(_context.PotSize * THREE_QUARTERS_POT),
-                        _context.PlayerBalance
-                    };
-                    _context.RaiseOptionsText = "1/2 POT|2/3 POT|3/4 POT|ALL IN";
-                }
-                else
-                {
-                    _context.RaiseOptions = new List<int> {
-                        (int)(_context.CurrentBet * RAISE_MULTIPLIER_2),
-                        (int)(_context.CurrentBet * RAISE_MULTIPLIER_3),
-                        (int)(_context.CurrentBet * RAISE_MULTIPLIER_4),
-                        _context.PlayerBalance
-                    };
-                    _context.RaiseOptionsText = "X2|X3|X4|ALL IN";
-                }
-            }
+            IRaiseOptionsStrategy strategy = _context.CurrentGameState == GameState.PreFlop
+                ? new PreFlopRaiseStrategy()
+                : new PostFlopRaiseStrategy();
+
+            var result = strategy.GetOptions(_context);
+
+            _context.RaiseOptions = result.Options;
+            _context.RaiseOptionsText = result.Text;
 
             _context.UpdateCallCheckButtonText();
             _context.IsRaiseMenuVisible = true;
@@ -111,9 +72,7 @@ namespace TexasHoldemWPF.ViewModels
             ExecuteRaiseAmount(raiseAmount);
 
             if (_context.PlayerBalance == 0)
-            {
                 await HandleAllInRaise();
-            }
             else
             {
                 _raiseOccurredInRound = true;
@@ -122,19 +81,84 @@ namespace TexasHoldemWPF.ViewModels
             }
         }
 
-        private bool IsInvalidRaise(int raiseAmount)
-        {
-            return raiseAmount <= 0 || raiseAmount > _context.PlayerBalance;
-        }
+        private bool IsInvalidRaise(int raiseAmount) => raiseAmount <= 0 || raiseAmount > _context.PlayerBalance;
 
         private void ExecuteRaiseAmount(int raiseAmount)
         {
-            _context.PlayerBalance -= raiseAmount;
-            _context.PotSize += raiseAmount;
+            UpdateBalances(raiseAmount);
             _context.CurrentBet += raiseAmount;
+            UpdatePlayerAction(0, _context.CurrentBet, $"Raise ${raiseAmount}");
+        }
+
+        public void Call()
+        {
+            if (!_context.CanAct) return;
+
+            int callAmount = Math.Min(CalculateCallAmount(), _context.PlayerBalance);
+
+            if (callAmount == 0)
+                _context.GetPlayer(0).LastAction = "Check";
+            else
+                ExecuteCall(callAmount);
+
+            FinalizeCall();
+        }
+
+        private int CalculateCallAmount() => _context.CurrentBet - _context.GetPlayer(0).CurrentBet;
+
+        private void ExecuteCall(int amount)
+        {
+            _context.GetPlayer(0).CurrentBet += amount;
+            _context.PlayerBalance -= amount;
+            _context.PotSize += amount;
+            _context.GetPlayer(0).LastAction = $"Call ${amount}";
+        }
+
+        private void FinalizeCall()
+        {
             _context.GetPlayer(0).Balance = _context.PlayerBalance;
-            _context.GetPlayer(0).CurrentBet = _context.CurrentBet;
-            _context.GetPlayer(0).LastAction = $"Raise ${raiseAmount}";
+            _context.UpdateCallCheckButtonText();
+            _context.CanAct = false;
+            _ = _context.BotManager.StartBotActions();
+        }
+
+        private void UpdateBalances(int amount)
+        {
+            _context.PlayerBalance -= amount;
+            _context.PotSize += amount;
+        }
+
+        private void UpdatePlayerAction(int index, int bet, string actionText)
+        {
+            var player = _context.GetPlayer(index);
+            player.Balance = _context.PlayerBalance;
+            player.CurrentBet = bet;
+            player.LastAction = actionText;
+            _context.UpdateCallCheckButtonText();
+        }
+        public void Fold()
+        {
+            if (!_context.CanAct) return;
+            _context.CanAct = false;
+            _context.GetPlayer(0).IsFolded = true;
+            _context.GetPlayer(0).LastAction = "Fold";
+
+            var activeBots = GetActiveBots();
+            if (activeBots.Count == 1)
+            {
+                HandleSingleBotRemaining(activeBots.First());
+                return;
+            }
+            _context.WinnerManager.DetermineWinnerAfterFold();
+        }
+
+        private List<Player> GetActiveBots() => _context.Players.Where(p => !p.IsFolded && p is BotPlayer).ToList();
+
+        private void HandleSingleBotRemaining(Player winner)
+        {
+            winner.Balance += _context.PotSize;
+            _context.ShowGameMessage($"{winner.Name} wins ${_context.PotSize} (all others folded)");
+            _context.RoundManager.StartNewRound();
         }
 
         private async Task HandleAllInRaise()
@@ -155,76 +179,6 @@ namespace TexasHoldemWPF.ViewModels
             _context.CurrentGameState = GameState.River;
             await Task.Delay(GameViewModel.STATE_TRANSITION_DELAY_MS * 10);
             _context.WinnerManager.EndGame();
-        }
-
-        public void Call()
-        {
-            if (!_context.CanAct) return;
-
-            int callAmount = CalculateCallAmount();
-            callAmount = Math.Min(callAmount, _context.PlayerBalance);
-
-            if (callAmount == 0)
-            {
-                _context.GetPlayer(0).LastAction = "Check";
-            }
-            else
-            {
-                ExecuteCall(callAmount);
-            }
-
-            FinalizeCall();
-        }
-
-        private int CalculateCallAmount()
-        {
-            return _context.CurrentBet - _context.GetPlayer(0).CurrentBet;
-        }
-
-        private void ExecuteCall(int amount)
-        {
-            _context.GetPlayer(0).CurrentBet += amount;
-            _context.PlayerBalance -= amount;
-            _context.PotSize += amount;
-            _context.GetPlayer(0).LastAction = $"Call ${amount}";
-        }
-
-        private void FinalizeCall()
-        {
-            _context.GetPlayer(0).Balance = _context.PlayerBalance;
-            _context.UpdateCallCheckButtonText();
-            _context.CanAct = false;
-            _ = _context.BotManager.StartBotActions();
-        }
-
-        public void Fold()
-        {
-            if (!_context.CanAct) return;
-
-            _context.CanAct = false;
-            _context.GetPlayer(0).IsFolded = true;
-            _context.GetPlayer(0).LastAction = "Fold";
-
-            var activeBots = GetActiveBots();
-            if (activeBots.Count == 1)
-            {
-                HandleSingleBotRemaining(activeBots.First());
-                return;
-            }
-
-            _context.WinnerManager.DetermineWinnerAfterFold();
-        }
-
-        private List<Player> GetActiveBots()
-        {
-            return _context.Players.Where(p => !p.IsFolded && p is BotPlayer).ToList();
-        }
-
-        private void HandleSingleBotRemaining(Player winner)
-        {
-            winner.Balance += _context.PotSize;
-            _context.ShowGameMessage($"{winner.Name} wins ${_context.PotSize} (all others folded)");
-            _context.RoundManager.StartNewRound();
         }
 
         public bool RaiseOccurredInRound => _raiseOccurredInRound;
