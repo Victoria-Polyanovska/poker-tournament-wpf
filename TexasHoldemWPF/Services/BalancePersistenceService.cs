33912907eeab433e7f8c377b71cca7af6d3809eb
Victoria-Polyanovska <ipz242_pve@student.ztu.edu.ua>
2026-05-18 19:56:21 +0300

    refactor: clean BalancePersistenceService

--- TexasHoldemWPF/Services/BalancePersistenceService.cs
+++ TexasHoldemWPF/Services/BalancePersistenceService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Security.Cryptography;
-using System.Text;
+﻿using System.IO;
 using Newtonsoft.Json;
 
 namespace TexasHoldemWPF.Services
@@ -11,8 +8,13 @@ namespace TexasHoldemWPF.Services
         private const string BalanceFile = "playerBalance.json";
         private const double DefaultBalance = 10000;
 
-        private static readonly byte[] AesKey = Encoding.UTF8.GetBytes("k9H2pLm4Wq8ZxRt7");
-        private static readonly byte[] AesIV = Encoding.UTF8.GetBytes("J3mN7vBt1QsPfYl9");
+        private readonly ICryptoProvider _cryptoProvider;
+
+        // Новий конструктор, який приймає інтерфейс шифрування
+        public BalancePersistenceService(ICryptoProvider cryptoProvider)
+        {
+            _cryptoProvider = cryptoProvider;
+        }
 
         public double Load()
         {
@@ -20,45 +22,15 @@ namespace TexasHoldemWPF.Services
                 return DefaultBalance;
 
             var encrypted = File.ReadAllText(BalanceFile);
-            var decrypted = Decrypt(encrypted);
+            var decrypted = _cryptoProvider.Decrypt(encrypted); // Викликаємо провайдер
             return JsonConvert.DeserializeObject<double>(decrypted);
         }
 
         public void Save(double balance)
         {
             var json = JsonConvert.SerializeObject(balance);
-            var encrypted = Encrypt(json);
+            var encrypted = _cryptoProvider.Encrypt(json); // Викликаємо провайдер
             File.WriteAllText(BalanceFile, encrypted);
-        }
-
-        private static string Encrypt(string plainText)
-        {
-            var aes = Aes.Create();
-            aes.Key = AesKey;
-            aes.IV = AesIV;
-            var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-
-            var ms = new MemoryStream();
-            using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-            using (var sw = new StreamWriter(cs))
-            {
-                sw.Write(plainText);
-            }
-            return Convert.ToBase64String(ms.ToArray());
-        }
-
-        private static string Decrypt(string cipherText)
-        {
-            var buffer = Convert.FromBase64String(cipherText);
-            var aes = Aes.Create();
-            aes.Key = AesKey;
-            aes.IV = AesIV;
-            var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-
-            var ms = new MemoryStream(buffer);
-            var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
-            var sr = new StreamReader(cs);
-            return sr.ReadToEnd();
         }
     }
 }
